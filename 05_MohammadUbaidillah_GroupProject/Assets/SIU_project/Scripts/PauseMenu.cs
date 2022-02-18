d79007ee0845134cd7bbbe31c2c68f0799a19779
utsubyo <97504303+15-junyuan@users.noreply.github.com>
2022-02-18 13:37:56 +0800

    unity pausemenu
    added pausemenu on gamescene
    added scripts for pausemenu and button

---
+++ 05_MohammadUbaidillah_GroupProject/Assets/SIU_project/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //check whether Game is pause
+    public static bool Gameispaused = false;
+
+    //Gameobject for pausemenu ui
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Gameispaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        //toggle pause menu ui to on / off
+        pauseMenuUI.SetActive(false);
+        //change time scale
+        Time.timeScale = 1f;
+        Gameispaused = false;
+    }
+
+    void Pause ()
+    {
+        //toggle pause menu ui to on / off
+        pauseMenuUI.SetActive(true);
+        //change time scale
+        Time.timeScale = 0f;
+        Gameispaused = true;
+    }
+
+    public void MenuLoad()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
