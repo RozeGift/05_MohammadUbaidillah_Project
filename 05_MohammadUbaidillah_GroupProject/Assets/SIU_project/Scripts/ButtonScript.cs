58809a45e467ec9693ef09c5eac231da0e51e2ad
utsubyo <97504303+15-junyuan@users.noreply.github.com>
2022-02-21 11:52:34 +0800

    add audio
    added audio
    add button for menu

---
+++ 05_MohammadUbaidillah_GroupProject/Assets/SIU_project/Scripts/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonScript : MonoBehaviour
+{
+    public void Playbutton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitButtton()
+    {
+        Application.Quit();
+    }
+
+
+}
